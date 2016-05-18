7b36b9d4930fd78a7be58c95280143d47a2c291b
Matthew Gutierrez <fatchicken007@gmail.com>
2016-05-18 13:34:03 -0700

    Add gulp task for bower components

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var files   = require('main-bower-files')();
 
 var DEST = 'dist/';
 
 gulp.task('build-js', function() {
-  return gulp.src('./public/**/*.js')
+  return gulp.src(['public/**/*.js','!public/bower_components/**/*.js'])
     .pipe(plugins.concat('/js/main.js'))
     .pipe(gulp.dest(DEST))
     .pipe(plugins.uglify())
@@ -13,7 +14,7 @@ gulp.task('build-js', function() {
 });
 
 gulp.task('build-css', function() {
-  return gulp.src('./public/**/*.css')
+  return gulp.src('public/**/*.css')
     .pipe(plugins.concat('/css/main.css'))
     .pipe(gulp.dest(DEST))
     .pipe(plugins.cssnano())
@@ -22,23 +23,32 @@ gulp.task('build-css', function() {
 });
 
 gulp.task('build-html', function() {
-  return gulp.src('./public/**/*.html')
+  return gulp.src('public/**/*.html')
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('build-components', function() {
+  return gulp.src(files)
+    .pipe(plugins.concat('/js/components.js'))
+    .pipe(gulp.dest(DEST))
+    .pipe(plugins.uglify())
+    .pipe(plugins.rename({ extname: '.min.js' }))
     .pipe(gulp.dest(DEST));
 });
 
 gulp.task('serve', function() {
   plugins.nodemon({
     script: 'server.js',
-    ignore: ['./public', './dist']
+    ignore: ['public', 'dist']
   });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./public/**/*.js', ['build-js']);
-  gulp.watch('./public/**/*.css', ['build-css']);
-  gulp.watch('./public/**/*.html', ['build-html']);
+  gulp.watch('public/**/*.js', ['build-js']);
+  gulp.watch('public/**/*.css', ['build-css']);
+  gulp.watch('public/**/*.html', ['build-html']);
 });
 
-gulp.task('build', ['build-js', 'build-css', 'build-html']);
+gulp.task('build', ['build-js', 'build-css', 'build-html', 'build-components']);
 
 gulp.task('default', ['watch', 'build', 'serve']);
