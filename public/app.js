6b6f6a9e35e6891a1e3ad5a8b00b8e0df520f746
Carlos Velasquez <cv.velasquez@gmail.com>
2016-05-24 20:27:20 -0700

    tuesNight

--- public/app.js
+++ public/app.js
@@ -6,19 +6,35 @@ function configuration($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
 
   $stateProvider
-    .state('search', {
+    .state('search1', {
       url: '',
       templateUrl: 'partials/search.html',
       controller: function($scope) {}
     })
-   .state('list', {
+    $stateProvider
+    .state('search2', {
+      url: '',
+      templateUrl: 'partials/search.html',
+      controller: function($scope) {}
+    })
+   .state('list1', {
       url: '/results?s',
-      templateUrl: 'partials/list.html',
+      templateUrl: 'partials/list1.html',
       controller: 'ListController as ctrl'
     })
-    .state('player', {
-      url: '/players/:id',
-      templateUrl: 'partials/player.html',
+   .state('list2', {
+      url: '/results?s',
+      templateUrl: 'partials/list2.html',
+      controller: 'ListController as ctrl'
+    })
+    .state('player1', {
+      url: '/players1/:id',
+      templateUrl: 'partials/player1.html',
+      controller: 'PlayerController as ctrl'
+    })
+    .state('player2', {
+      url: '/players2/:id',
+      templateUrl: 'partials/player2.html',
       controller: 'PlayerController as ctrl'
     });
 }
