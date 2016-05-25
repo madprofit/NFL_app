a66111ed6b68d8e93880ffa25706d6cfb297bf8f
Carlos Velasquez <cv.velasquez@gmail.com>
2016-05-24 19:46:04 -0700

    t7

--- public/js/listController.js
+++ public/js/listController.js
@@ -11,7 +11,7 @@ function ListController($http, $stateParams) {
     method: 'GET',
     url: 'http://www.fantasyfootballnerd.com/service/players/json/test/QB/'
   }).then(function successCallback(response) {
-    self.results = response.data.Players;
+    self.results = response.data.Players[0];
     console.log(self.results);
     // self.results.Players.forEach(function(player){
     //   console.log(player.displayName)
