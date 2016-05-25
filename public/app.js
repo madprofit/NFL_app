3e5c9dee761943369923cd5e33f9a382b2d0e63f
Carlos Velasquez <cv.velasquez@gmail.com>
2016-05-25 14:19:40 -0700

    uiview

--- public/app.js
+++ public/app.js
@@ -6,35 +6,31 @@ function configuration($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
 
   $stateProvider
-    .state('search1', {
+    .state('search1',  {
       url: '',
-      templateUrl: 'partials/search.html',
+      templateUrl: 'partials/search1.html',
       controller: function($scope) {}
     })
-    $stateProvider
-    .state('search2', {
-      url: '',
-      templateUrl: 'partials/search.html',
-      controller: function($scope) {}
+      // $stateProvider
+      // .state('search2', {
+      //   url: '',
+      //   templateUrl: 'partials/search2.html',
+      //   controller: function($scope) {}
+      // })
+      .state('list1', {
+         url: '/results1/:s',
+         templateUrl: 'partials/list1.html',
+         controller: 'ListController1 as ctrl1'
     })
-   .state('list1', {
-      url: '/results?s',
-      templateUrl: 'partials/list1.html',
-      controller: 'ListController as ctrl'
-    })
-   .state('list2', {
-      url: '/results?s',
-      templateUrl: 'partials/list2.html',
-      controller: 'ListController as ctrl'
-    })
-    .state('player1', {
-      url: '/players1/:id',
-      templateUrl: 'partials/player1.html',
-      controller: 'PlayerController as ctrl'
-    })
-    .state('player2', {
-      url: '/players2/:id',
-      templateUrl: 'partials/player2.html',
-      controller: 'PlayerController as ctrl'
+      .state('list2', {
+         url: '/results2/:s',
+         templateUrl: 'partials/list1.html',
+         controller: 'ListController2 as ctrl2'
+       })
+      .state('player1', {
+        url: '/players1/:id',
+        templateUrl: 'partials/player1.html',
+        controller: 'PlayerController as ctrl'
     });
+
 }
--- public/js/listController2.js
+++ public/js/listController2.js
@@ -1,19 +1,19 @@
 angular
   .module('nflApp')
-  .controller('ListController1', ListController1);
+  .controller('ListController2', ListController2);
 
-function ListController1($http, $stateParams) {
+function ListController2($http, $stateParams) {
   var self = this;
   var searchString = ($stateParams.s);
   self.searchString = searchString;
-  console.log($stateParams.s)
+  console.log($stateParams.s);
   var qb = [];
 
   $http({
     method: 'GET',
     url: 'http://www.fantasyfootballnerd.com/service/players/json/test/QB/'
-  }).then(function successCallback(response1) {
-    self.results = response1.data.Players;
+  }).then(function successCallback(response2) {
+    self.results = response2.data.Players;
     console.log(self.results);
     // self.results.Players.forEach(function(player){
     //   console.log(player.displayName)
